3fdaae711fa3b2325445c052e5f6cdd1dc30df1e
Peter deHaan <peter@deseloper.com>
2018-03-16 17:01:45 -0700

    Fix ESLint errors and lint HTML files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   env: {
+    browser: true,
     es6: true,
     webextensions: true
   },
@@ -7,7 +8,26 @@ module.exports = {
     "eslint:recommended",
     "plugin:mozilla/recommended"
   ],
+  globals: {
+    addMessageListener: false,
+    sendAsyncMessage: false,
+    content: false
+  },
+  plugins: [
+    "json",
+    "mozilla"
+  ],
   root: true,
   rules: {
+    "mozilla/use-chromeutils-import": "off",
+
+    "eqeqeq": "error",
+    "no-global-assign": "warn",
+    "no-implicit-globals": "off",
+    "no-return-await": "warn",
+    "no-var": "error",
+    "no-warning-comments": "warn",
+    "prefer-const": "error",
+    "quotes": ["error", "double"]
   }
 };
